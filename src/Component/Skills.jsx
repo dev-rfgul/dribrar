18a95a147560633f46e17d22a4df7ecd5153275f
dev-rfgul <rfgul587@gmail.com>
2024-10-21 23:34:17 +0500

    added links in skills section

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -7,26 +7,31 @@ const skills = [
         title: 'Animal Feed Formulation',
         description: 'Veterinary support meals for specific medical needs like renal, cardiac, and obesity management.',
         icon: faPaw,
+        url:'https://www.upwork.com/services/product/lifestyle-balanced-homemade-dog-food-formulation-1552934708666601472?ref=project_share '
     },
     {
         title: 'Content Review and Fact-Checking',
         description: 'Meticulous review for animal nutrition content, ensuring scientific accuracy.',
         icon: faMagnifyingGlass,
+        url:'https://www.upwork.com/services/product/writing-translation-fact-checked-veterinary-content-blog-book-poster-white-paper-1847589635253997687?ref=project_share '
     },
     {
         title: 'Veterinary Content Writer',
         description: 'Creating scientifically accurate content focused on veterinary nutrition and treatments.',
         icon: faBook,
+        url:'https://www.upwork.com/services/product/writing-translation-pets-related-content-article-and-blogs-1552928466689769472?ref=project_share '
     },
     {
         title: 'Blog Host Writer',
         description: 'Hosting and writing for veterinary blogs with research-driven articles.',
         icon: faPen,
+        url:'https://www.fiverr.com/s/1qVYd36 '
     },
     {
         title: 'Professional Translator',
         description: 'Translation services in English, Urdu, and Turkish, specializing in veterinary topics.',
         icon: faGlobe,
+        url:'https://www.fiverr.com/s/2Kyg45q '
     },
 ];
 
@@ -47,8 +52,8 @@ const SkillsSection = () => {
                             <div className='flex justify-center items-center flex-col'>
                                 <h3 className="text-xl font-semibold text-gray-700 mb-4">{skill.title}</h3>
                                 <p className="text-gray-600 mb-6 text-center">{skill.description}</p>
-                                <button className="py-2 px-6 border border-black rounded-lg text-black bg-white hover:bg-gray-900 hover:text-white transition-transform transform hover:scale-105">
-                                    <a href="">
+                                <button className="py-2 px-6 border border-black rounded-lg text-black bg-white hover:bg-gray-900 hover:text-white transition-transform transform hover:scale-105" >
+                                    <a  target='_blank' href={skill.url}>
                                         Contact Me
                                     </a>
                                 </button>
